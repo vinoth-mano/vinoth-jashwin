f0210a05969ef5c4ed8e64dd49971943ee53a349
vinoth <vinothmanoharan2507@gmail.com>
2019-11-14 02:09:01 +0530

    First Commit

---
+++ demo/src/main/java/com/example/sampleweb/Test.java
@@ -0,0 +1,14 @@
+package com.example.sampleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		
+	ArrayList <String> dd= new ArrayList();
+	
+	}
+
+}
